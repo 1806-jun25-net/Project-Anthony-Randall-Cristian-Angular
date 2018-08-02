0dd2b3a131fa3a9700ba3b7e9366fb1b76563d8f
ajzoell <ajzoellner@gmail.com>
2018-08-02 19:59:48 -0400

    created dashboard component

--- project2-angular/src/app/app.module.ts
+++ project2-angular/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '../../node_modules/@angular/router';
 import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
