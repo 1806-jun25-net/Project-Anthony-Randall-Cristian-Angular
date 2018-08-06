653c675ffe3450322a07a6f9d31de5104360e2f2
ajzoell <ajzoellner@gmail.com>
2018-08-06 12:56:32 -0400

    test

--- project2-angular/src/app/app-routing.module.ts
+++ project2-angular/src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ import { SuperOrderComponent } from './super-order/super-order.component';
 
 const appRoutes: Routes =[
   {path: "", component: LoginComponent },
-  {path: "dashboard", component: DashboardComponent },
+  {path: "https://project2angular.azurewebsites.net/dashboard", component: DashboardComponent },
   {path: "Locations", component: LocationsComponent},
   {path: "Users", component: UsersComponent},
   {path: "detail/:username", component: UserDetailsComponent},
